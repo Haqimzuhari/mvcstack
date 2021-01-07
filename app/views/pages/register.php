922ab937d8f0c36d2214d0d1b31d44b65a5a18c7
haqimzuhari <mhaqimzuhari@gmail.com>
2021-01-07 13:56:23 +0800

    Install TailwindCSS into machine Apply @apply on TailwindCSS

--- app/views/pages/register.php
+++ app/views/pages/register.php
@@ -1,12 +1,20 @@
-<section class="min-h-screen flex items-center">
+<section class="min-h-screen flex items-center justify-center">
 
-    <div class="w-full max-w-2xl flex flex-col px-32 py-5">
+    <div class="w-full max-w-sm flex flex-col p-10 bg-white shadow-sm rounded-lg">
 
         <div class="flex flex-col gap-5">
 
-            <div class="flex items-start gap-2 text-sm">
+            <div>
+                <a href="<?= route('') ?>" class="stack-link secondary text-xs">
+                    Cancel
+                </a>
+            </div>
+            
+            <h1 class="font-bold text-xl">REGISTER AN ACCOUNT</h1>
+
+            <div class="flex items-start gap-2 text-xs">
                 <p>HAVE AN ACCOUNT?</p>
-                <a href="<?=route('login')?>" class="transition duration-300 ease-in-out font-semibold text-indigo-400 hover:text-indigo-500 cursor-pointer focus:outline-none border-b-4 border-transparent hover:border-indigo-400">
+                <a href="<?=route('login')?>" class="stack-link primary">
                     LOGIN NOW
                 </a>
             </div>
@@ -14,32 +22,30 @@
             <!-- Create account form -->
             <form class="flex flex-col gap-5" method="post" action="<?=route('register')?>" x-data="{password:false, confirm:false}">
 
-                <h1 class="font-bold text-lg">CREATE AN ACCOUNT</h1>
-
                 <div class="flex flex-col gap-1">
-                    <label for="email" class="text-xs font-light text-gray-600">EMAIL</label>
-                    <input type="email" name="email" id="email" placeholder="Email" class="w-full tracking-widest appearance-none transition duration-300 ease-in-out bg-gray-50 focus:outline-none focus:bg-white px-3 py-2 text-xs border border-transparent focus:border-indigo-500 rounded-sm placeholder-gray-500 text-gray-800" />
+                    <label for="email" class="stack-form-label">EMAIL</label>
+                    <input type="email" name="email" id="email" class="stack-form primary" autofocus />
                 </div>
 
                 <div class="flex flex-col gap-1">
-                    <label for="first_name" class="text-xs font-light text-gray-600">FIRST NAME</label>
-                    <input type="text" name="first_name" id="first_name" placeholder="First Name" class="w-full tracking-widest appearance-none transition duration-300 ease-in-out bg-gray-50 focus:outline-none focus:bg-white px-3 py-2 text-xs border border-transparent focus:border-indigo-500 rounded-sm placeholder-gray-500 text-gray-800" />
+                    <label for="first_name" class="stack-form-label">FIRST NAME</label>
+                    <input type="text" name="first_name" id="first_name" class="stack-form primary" />
                 </div>
 
                 <div class="flex flex-col gap-1">
-                    <label for="last_name" class="text-xs font-light text-gray-600">LAST NAME</label>
-                    <input type="text" name="last_name" id="last_name" placeholder="Last Name" class="w-full tracking-widest appearance-none transition duration-300 ease-in-out bg-gray-50 focus:outline-none focus:bg-white px-3 py-2 text-xs border border-transparent focus:border-indigo-500 rounded-sm placeholder-gray-500 text-gray-800" />
+                    <label for="last_name" class="stack-form-label">LAST NAME</label>
+                    <input type="text" name="last_name" id="last_name" class="stack-form primary" />
                 </div>
 
                 <div class="flex flex-col gap-1">
-                    <label for="contact" class="text-xs font-light text-gray-600">CONTACT NO.</label>
-                    <input type="number" name="contact" id="contact" placeholder="01XXXXXXXX" class="w-full tracking-widest appearance-none transition duration-300 ease-in-out bg-gray-50 focus:outline-none focus:bg-white px-3 py-2 text-xs border border-transparent focus:border-indigo-500 rounded-sm placeholder-gray-500 text-gray-800" />
+                    <label for="contact" class="stack-form-label">CONTACT NO.</label>
+                    <input type="number" name="contact" id="contact" class="stack-form primary" />
                 </div>
 
                 <div class="flex flex-col gap-1">
-                    <label for="password" class="text-xs font-light text-gray-600">PASSWORD</label>
+                    <label for="password" class="stack-form-label">PASSWORD</label>
                     <span class="w-full relative">
-                        <input type="password" name="password" id="password" placeholder="Password" class="w-full tracking-widest appearance-none transition duration-300 ease-in-out bg-gray-50 focus:outline-none focus:bg-white px-3 py-2 text-xs border border-transparent focus:border-indigo-500 rounded-sm placeholder-gray-500 text-gray-800" x-bind:type="password?'text':'password'" />
+                        <input type="password" name="password" id="password" class="stack-form primary" x-bind:type="password?'text':'password'" />
                         <span class="transition duration-300 ease-in-out absolute inset-y-0 right-0 flex items-center justify-center w-10 text-gray-400 hover:text-gray-600 cursor-pointer" x-on:click="password=!password">
                             <i class="far" x-bind:class="{'fa-eye':!password, 'fa-eye-slash':password}"></i>
                         </span>
@@ -48,10 +54,10 @@
                 </div>
 
                 <div class="flex flex-col gap-1">
-                    <label for="confirm_password" class="text-xs font-light text-gray-600">CONFIRM PASSWORD</label>
+                    <label for="confirm_password" class="stack-form-label">CONFIRM PASSWORD</label>
                     <span class="w-full relative">
-                        <input type="password" name="confirm_password" id="confirm_password" placeholder="Re-type Password" class="w-full tracking-widest appearance-none transition duration-300 ease-in-out bg-gray-50 focus:outline-none focus:bg-white px-3 py-2 text-xs border border-transparent focus:border-indigo-500 rounded-sm placeholder-gray-500 text-gray-800" x-bind:type="confirm?'text':'password'" />
-                        <span class="transition duration-300 ease-in-out absolute inset-y-0 right-0 flex items-center justify-center w-10 text-gray-400 hover:text-gray-600 cursor-pointer" x-on:click="confirm=!confirm">
+                        <input type="password" name="confirm_password" id="confirm_password" class="stack-form primary" x-bind:type="confirm?'text':'password'" />
+                        <span class="stack-button-basic absolute inset-y-0 right-0 flex items-center justify-center w-10 text-gray-400 hover:text-gray-600" x-on:click="confirm=!confirm">
                             <i class="far" x-bind:class="{'fa-eye':!confirm, 'fa-eye-slash':confirm}"></i>
                         </span>
                     </span>
@@ -59,7 +65,7 @@
                 </div>
 
                 <div>
-                    <button type="submit" name="register" class="tracking-widest transition duration-300 ease-in-out bg-indigo-400 hover:bg-indigo-500 shadow-sm hover:shadow-lg font-semibold px-3 py-2 rounded-sm text-xs text-gray-50">
+                    <button type="submit" name="register" class="stack-button primary">
                         CREATE ACCOUNT
                     </button>
                 </div>
