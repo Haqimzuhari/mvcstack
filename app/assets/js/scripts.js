d65c6864abec6e4e3dfe82cfdbb01bcefcf326c7
haqimzuhari <mhaqimzuhari@gmail.com>
2021-04-20 11:29:20 +0800

    Optimizing component function in framework Use Karla and Lora fonts

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -1,3 +1,25 @@
 if ( window.history.replaceState ) {
     window.history.replaceState( null, null, window.location.href );
 }
+
+
+function bsd(status) {
+	var body = document.querySelector("body");
+
+	var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+	var scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+
+	window.onscroll = function () {};
+
+	if (status === true) {
+		// Check window scroll exists else use traditional method
+		if (window.onscroll !== null) {
+			// if any scroll is attempted, set this to the previous value
+			window.onscroll = function () {
+				window.scrollTo(scrollLeft, scrollTop);
+			};
+		}
+	} else {
+		window.onscroll = function () {};
+	}
+}
