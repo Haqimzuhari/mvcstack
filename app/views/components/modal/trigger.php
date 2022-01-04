5b9a7ef7b2767672fd6301b74e61fd39219fd8fb
Haqim Zuhari <mhaqimzuhari@gmail.com>
2022-01-05 00:24:09 +0800

    Improve base design

---
+++ app/views/components/modal/trigger.php
@@ -0,0 +1,3 @@
+<span x-data="{id:'<?=$target?>'}" x-on:click="bsd(true), $dispatch('modal-overlay',{id})">
+    @content
+</span>
--- app/views/pages/register.php
+++ app/views/pages/register.php
@@ -1,74 +1,35 @@
-<section class="flex items-center justify-center min-h-screen">
-
-    <div class="flex flex-col w-full max-w-sm p-10 bg-white rounded-lg shadow-lg">
-
-        <div class="flex flex-col gap-5">
-            
-            <h1 class="text-xl font-bold">REGISTER AN ACCOUNT</h1>
-
-            <div class="flex items-start gap-2 text-xs">
-                <p>HAVE AN ACCOUNT?</p>
-                <a href="<?=route('login')?>" class="link primary">
-                    LOGIN NOW
-                </a>
-            </div>
-
-            <!-- Create account form -->
-            <form class="flex flex-col gap-5" method="post" action="<?=route('register')?>" x-data="{password:false, confirm:false}">
-
-                <div class="flex flex-col gap-1">
-                    <label for="email" class="input-label">EMAIL</label>
-                    <input type="email" name="email" id="email" class="form-input primary" autofocus />
-                </div>
-
-                <div class="flex flex-col gap-1">
-                    <label for="first_name" class="input-label">FIRST NAME</label>
-                    <input type="text" name="first_name" id="first_name" class="form-input primary" />
-                </div>
-
-                <div class="flex flex-col gap-1">
-                    <label for="last_name" class="input-label">LAST NAME</label>
-                    <input type="text" name="last_name" id="last_name" class="form-input primary" />
-                </div>
-
-                <div class="flex flex-col gap-1">
-                    <label for="contact" class="input-label">CONTACT NO.</label>
-                    <input type="number" name="contact" id="contact" class="form-input primary" />
-                </div>
-
-                <div class="flex flex-col gap-1">
-                    <label for="password" class="input-label">PASSWORD</label>
-                    <span class="relative w-full">
-                        <input type="password" name="password" id="password" class="form-input primary" x-bind:type="password?'text':'password'" />
-                        <span class="absolute inset-y-0 right-0 flex items-center justify-center w-10 text-gray-400 transition duration-300 ease-in-out cursor-pointer hover:text-gray-600 focus:outline-none" x-on:click="password=!password">
-                            <i x-bind:class="{'las la-eye':!password, 'las la-low-vision':password}"></i>
-                        </span>
-                    </span>
-                    </span>
-                </div>
-
-                <div class="flex flex-col gap-1">
-                    <label for="confirm_password" class="input-label">CONFIRM PASSWORD</label>
-                    <span class="relative w-full">
-                        <input type="password" name="confirm_password" id="confirm_password" class="form-input primary" x-bind:type="confirm?'text':'password'" />
-                        <span class="absolute inset-y-0 right-0 flex items-center justify-center w-10 text-gray-400 transition duration-300 ease-in-out cursor-pointer hover:text-gray-600 focus:outline-none" x-on:click="confirm=!confirm">
-                            <i x-bind:class="{'las la-eye':!confirm, 'las la-low-vision':confirm}"></i>
-                        </span>
-                    </span>
-                    </span>
-                </div>
-
-                <div>
-                    <button type="submit" name="register" class="button primary">
-                        CREATE ACCOUNT
-                    </button>
-                </div>
-
-            </form>
-            <!-- /Create account form -->
-
+<section class="min-h-screen grid place-items-center">
+    <div class="w-full max-w-md bg-white rounded-lg shadow-xl p-8 grid grid-cols-1 gap-8">
+        <div>
+            <h1 class="font-bold text-2xl">Register</h1>
         </div>
 
-    </div>
+        <div class="flex space-x-2 text-sm">
+            <p>Already have an account?</p>
+            <a href="<?= route('login') ?>" class="text-blue-600 hover:text-blue-500 transition duration-300 ease-in-out">Login Here</a>
+        </div>
 
+        <form method="post" action="<?=route('register')?>" class="grid grid-cols-1 gap-8">
+            <div class="grid grid-cols-1 gap-3">
+                <div class="grid grid-cols-1">
+                    <label>Email Address</label>
+                    <input type="email" name="email_address" value="<?= $this->email_address ?? '' ?>" class="appearance-none py-2 px-4 bg-zinc-100 border border-zinc-100 rounded hover:bg-white transition duration-300 ease-in-out"/>
+                </div>
+    
+                <div class="grid grid-cols-1">
+                    <label>Password</label>
+                    <input type="password" name="password" class="appearance-none py-2 px-4 bg-zinc-100 border border-zinc-100 rounded hover:bg-white transition duration-300 ease-in-out"/>
+                </div>
+    
+                <div class="grid grid-cols-1">
+                    <label>Confirm Password</label>
+                    <input type="password" name="password_confirmation" class="appearance-none py-2 px-4 bg-zinc-100 border border-zinc-100 rounded hover:bg-white transition duration-300 ease-in-out"/>
+                </div>
+            </div>
+
+            <div class="grid grid-cols-1">
+                <button type="submit" name="register" class="py-2 text-center bg-blue-200 text-blue-800 hover:bg-blue-300 transition duration-300 ease-in-out focus:outline-none font-bold rounded">Register</button>
+            </div>
+        </form>
+    </div>
 </section>
